bbf6fe301986dc1a81c912269c18ebd6aefd7215
mac-kenz0 <jordanm8118@gmail.com>
2017-04-16 12:21:28 -0400

    creating repository to refamilirize self with java and eclipse.

---
+++ reverse-a-string/src/reversedMethods.java
@@ -0,0 +1,26 @@
+public class reversedMethods {
+	
+	/* Brute Force Algorithm
+	 * Time complexity - 
+	 * Space complexity - 
+	 * */
+	public static String bruteForce(String s) {
+		
+		String reversed = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			reversed += s.charAt(i);
+		}
+		return reversed;
+}
+	/* TODO: Create methods with more elegant solutions
+	 * is there a "reverse" method in java ?
+	 * is there any way to do this problem without creating a new variable ?
+	 * Maybe use temp instead....
+	 * Research this problem
+	 * */
+
+	
+	
+	
+	
+}
